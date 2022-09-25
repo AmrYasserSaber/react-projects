8dfe7091fbb10a7375104e4d3f454d25e86c2912
Amr Yasser Saber <amr.yassr.2@gmail.com>
2022-09-25 15:45:13 -0700

    project-4

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const question = (question)=>{
+    return(
+      <SingleQuestion key={question.id}{...question} />
+    );
+  }
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {data.map(question)}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
